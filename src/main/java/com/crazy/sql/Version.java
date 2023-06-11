a431101faf83a03a0b104b6f3c34f60308e70af4
Akiba no ichiichiyoha <2091916141@qq.com>
2023-06-11 13:20:45 +0800

    版本更新

--- src/main/java/com/crazy/sql/Version.java
+++ src/main/java/com/crazy/sql/Version.java
@@ -2,6 +2,6 @@ package com.crazy.sql;
 
 public class Version {
     public static void main(String[] args) {
-        System.out.println("CrazySQL:1.4.2");
+        System.out.println("CrazySQL:1.4.3");
     }
 }
